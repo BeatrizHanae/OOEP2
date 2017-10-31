eceb0216e780cdf7773e01bd04ef856b2106e331
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-10-31 14:33:59 -0200

    Ativando o botão de FPF

--- src/controller/ActionFPF.java
+++ src/controller/ActionFPF.java
@@ -3,12 +3,20 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
 import view.FPF_simulacao;
 
-public class actionFPF implements ActionListener {
-	public void actionPerformed(ActionEvent e) {
-		FPF_simulacao FPF1 = new FPF_simulacao();
-		FPF1.setVisible(true);
+public class ActionFPF implements ActionListener {
 	
+	private JFrame frameMenu;
+	
+	public ActionFPF (JFrame frameMenu) {
+		this.frameMenu = frameMenu;
+	}
+	public void actionPerformed(ActionEvent e) {
+		frameMenu.setVisible(false);
+		FPF_simulacao telaFPF = new FPF_simulacao();
+		telaFPF.setVisible(true);
 	}
 };
