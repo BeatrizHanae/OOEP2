3e81a16936e978c5298ae65dcd76575f1d642b42
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-01 03:09:04 -0200

    Criando a Main para JFrame

---
+++ src/view/Main.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+	public static void main(String [] args) throws IOException{
+		JFrame telaInicial = new JFrame();
+		telaInicial = new JFrame("APRENDER QEE!");
+		telaInicial.setSize(1000, 500);
+		telaInicial.setLocationRelativeTo(null);
+		telaInicial.setResizable(true);
+		new Menu(telaInicial);
+		
+		
+	}
+}
