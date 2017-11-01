e82e608b0b132e69ec46dfead23801f27d65f12d
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-01 03:09:53 -0200

    Herdando a JFrame da Main

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,74 +1,74 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Font;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import controller.ActionDH;
-import controller.ActionFPF;
-
-import javax.swing.JTextField;
 import javax.swing.JLabel;
-import java.awt.Font;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import controller.ActionDH;
+import controller.ActionMenu;
 
-public class menu extends JFrame {
-
-	private JPanel contentPane;
+public class Menu {
+	
 	private JLabel lblNewLabel;
-	private static final long serialVersionUID = 1L;
-	private JFrame Menu;
+	private JPanel painelInicial;
+	
+	public Menu(JFrame telaInicial) throws IOException{
+		
+		criaPainelInicial(telaInicial);
 
-	/**
-	 * Launch the application.
-	 */
-	
-	public static void main(String[] args) {
-		menu frame = new menu();
-		frame.setVisible(true);
 	}
+
 	
-	public menu() {
+	public void criaPainelInicial(JFrame telaInicial) throws IOException{
+		
+		
+		painelInicial = new JPanel();
+		painelInicial.setLayout(null);
+		painelInicial.setLocation((telaInicial.getWidth()-painelInicial.getWidth())/2,(telaInicial.getHeight()-painelInicial.getHeight())/2);
+		
 		setTitle("APRENDA QEE!");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		telaInicial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
 		lblNewLabel = new JLabel("Bem vindo à  APRENDA QEE!");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
 		lblNewLabel.setBounds(40, 32, 208, 16);
-		contentPane.add(lblNewLabel);
+		painelInicial.add(lblNewLabel);
 		
 		JLabel lblEscolhaQualA = new JLabel("Escolha qual a simulação que deseja prosseguir:");
 		lblEscolhaQualA.setHorizontalAlignment(SwingConstants.CENTER);
 		lblEscolhaQualA.setBounds(40, 60, 312, 16);
-		contentPane.add(lblEscolhaQualA);
+		painelInicial.add(lblEscolhaQualA);
 		
 		JButton btnDistoroHarmnica = new JButton("DISTORÇÃO HARMÔNICA");
-		btnDistoroHarmnica.addActionListener(new ActionDH(Menu));
+		btnDistoroHarmnica.addActionListener(new ActionDH(telaInicial));
 		btnDistoroHarmnica.setBounds(73, 159, 305, 29);
-		contentPane.add(btnDistoroHarmnica);
+		telaInicial.getContentPane().add(painelInicial);
+		painelInicial.add(btnDistoroHarmnica);
 		
 		JButton btnFluxoDePotncia = new JButton("FLUXO DE POTÊNCIA FUNDAMENTAL");
-		btnFluxoDePotncia.addActionListener(new ActionFPF(Menu));
+		btnFluxoDePotncia.addActionListener(new ActionMenu(telaInicial));
 		btnFluxoDePotncia.setBounds(73, 104, 305, 29);
-		contentPane.add(btnFluxoDePotncia);
+		telaInicial.getContentPane().add(painelInicial);
+		painelInicial.add(btnFluxoDePotncia);
 		
 		JButton btnNewButton = new JButton("FLUXO DE POTÊNCIA HARMÔNICO");
 		btnNewButton.setBounds(73, 214, 305, 29);
-		contentPane.add(btnNewButton);
+		telaInicial.getContentPane().add(painelInicial);
+		painelInicial.add(btnNewButton);
 		
+		telaInicial.setVisible(true);
+		
+	}
+
+	private void setBounds(int i, int j, int k, int l) {
+	}
+
+	private void setTitle(String string) {	
 	}
 }
