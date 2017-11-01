481714ef0747b757abce7b687287656a95e1faab
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-01 03:12:03 -0200

    Arrumando a função correta para os botões que simula o Fluxo de Potencia Fundamental na controller

--- src/controller/ActionFluxoPotenciaF.java
+++ src/controller/ActionFluxoPotenciaF.java
@@ -1,22 +1,3 @@
 package controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-
-import view.FPF_simulacao;
-
-public class ActionFPF implements ActionListener {
-	
-	private JFrame frameMenu;
-	
-	public ActionFPF (JFrame frameMenu) {
-		this.frameMenu = frameMenu;
-	}
-	public void actionPerformed(ActionEvent e) {
-		frameMenu.setVisible(false);
-		FPF_simulacao telaFPF = new FPF_simulacao();
-		telaFPF.setVisible(true);
-	}
-};
