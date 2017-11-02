17938ab504c0d9c56a857b7b2c14786dd5239afa
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-02 01:17:40 -0200

    Aumentando a tela quando inicializada

--- src/view/Main.java
+++ src/view/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String [] args) throws IOException{
 		JFrame telaInicial = new JFrame();
 		telaInicial = new JFrame("APRENDER QEE!");
-		telaInicial.setSize(1000, 500);
+		telaInicial.setSize(1000, 1000);
 		telaInicial.setLocationRelativeTo(null);
 		telaInicial.setResizable(true);
 		new Menu(telaInicial);
