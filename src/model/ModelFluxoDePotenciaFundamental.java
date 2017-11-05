124fdf161b91dc29d5c95d64757f6cd01e98d637
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-05 12:42:12 -0200

    Classe criada para relacionar as contas de Fluxo de Potencia Fundamental com a Controller e a View

--- src/model/ModelFluxoDePotenciaFundamental.java
+++ src/model/ModelFluxoDePotenciaFundamental.java
@@ -1,5 +1,29 @@
 package model;
 
-public class FluxoDePotenciaFundamental {
-
+public class ModelFluxoDePotenciaFundamental {
+	public double frequenciaAngular = 120*Math.PI;
+	
+	public double OndaTensao(double VRMS, double t, double angulov) {
+		return VRMS*Math.cos(frequenciaAngular*t + angulov);
+	}
+	public double OndaCorrente(double IRMS, double t, double anguloi) {
+		return IRMS*Math.cos(frequenciaAngular*t + anguloi);
+	}
+	public double OndaPotenciaInstanea(double VRMS, double t, double angulov, double anguloi, double IRMS) {
+		return OndaTensao(VRMS,t,angulov)*OndaCorrente(IRMS,t,anguloi);
+	}
+	public double ValorPotAtiva(double VRMS, double angulov, double anguloi, double IRMS) {
+		return VRMS*IRMS*Math.cos(angulov-anguloi);
+	}
+	public double ValorPotReativa(double VRMS, double angulov, double anguloi, double IRMS) {
+		return VRMS*IRMS*Math.sin(angulov - anguloi);
+	}
+	public double ValorPotAparente(double VRMS, double IRMS) {
+		return VRMS*IRMS;
+	}
+	public double ValorFatorPot(double angulov, double anguloi) {
+		return Math.cos(angulov-anguloi);
+	}
+	
+	
 }
