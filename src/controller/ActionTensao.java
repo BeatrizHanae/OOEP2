ef7a231e15694f0124ff43a2e9b98dbdcedc701c
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-11 23:42:51 -0200

    Botao para simular tensão esta funcionando e gerando o gráfico

--- src/controller/ActionTensao.java
+++ src/controller/ActionTensao.java
@@ -2,7 +2,6 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -14,9 +13,6 @@ import view.GraphPanel;
 
 public class ActionTensao implements ActionListener {
 	
-	private JPanel painelFPF;	
-	private JPanel painelTensao;
-	private JFrame telaInicial;
 	private JSpinner amplitude;
 	private JSpinner angulo;
 	public GraphPanel grafico1;
@@ -26,9 +22,6 @@ public class ActionTensao implements ActionListener {
 		public ActionTensao(JSpinner amplitude, JSpinner angulo,
 				GraphPanel grafico1, List<Double> lista1, JPanel painelTensao, JPanel painelFPF, JFrame telaInicial) {
 			super();
-			this.painelFPF = painelFPF;
-			this.painelTensao = painelTensao;
-			this.telaInicial = telaInicial;
 			this.amplitude = amplitude;
 			this.angulo = angulo;
 			this.grafico1 = grafico1;
@@ -45,8 +38,7 @@ public class ActionTensao implements ActionListener {
 			lista1.add(botaoSimularTensao.OndaTensao(amplitude1,t,angulo1));
 			grafico1.setScores(lista1);
 			}
-			painelFPF.setVisible(false);
-		
+			//painelFPF.setVisible(false);
 		}
 		
 };
