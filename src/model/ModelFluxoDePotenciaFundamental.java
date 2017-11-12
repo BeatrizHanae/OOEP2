4c82c501ea3b9f1448fd12a40ed715e08af3415c
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-12 06:04:34 -0200

    Contas da Onda da Tensao e da Corrente estão corretas

--- src/model/ModelFluxoDePotenciaFundamental.java
+++ src/model/ModelFluxoDePotenciaFundamental.java
@@ -10,25 +10,25 @@ public class ModelFluxoDePotenciaFundamental {
 	//VRMS = amplitude da Tensao
 	//IRMS = amplitude da Corrente
 	public double OndaTensao(double VRMS, double t, double angulov) {
-		return VRMS*Math.cos(frequenciaAngular*t + angulov);
+		return VRMS*Math.cos(Math.toRadians(frequenciaAngular*t) + angulov);
 	}
 	public double OndaCorrente(double IRMS, double t, double anguloi) {
-		return IRMS*Math.cos(frequenciaAngular*t + anguloi);
+		return IRMS*Math.cos(Math.toRadians(frequenciaAngular*t )+ anguloi);
 	}
 	public double OndaPotenciaInstanea(double VRMS, double t, double angulov, double anguloi, double IRMS) {
 		return OndaTensao(VRMS,t,angulov)*OndaCorrente(IRMS,t,anguloi);
 	}
 	public double ValorPotAtiva(double VRMS, double angulov, double anguloi, double IRMS) {
-		return VRMS*IRMS*Math.cos(angulov-anguloi);
+		return Math.round(VRMS*IRMS*Math.cos(Math.toRadians(angulov)-Math.toRadians(anguloi)));
 	}
 	public double ValorPotReativa(double VRMS, double angulov, double anguloi, double IRMS) {
-		return VRMS*IRMS*Math.sin(angulov - anguloi);
+		return Math.round(VRMS*IRMS*Math.sin(Math.toRadians(angulov) - Math.toRadians(anguloi)));
 	}
 	public double ValorPotAparente(double VRMS, double IRMS) {
-		return VRMS*IRMS;
+		return Math.round(VRMS*IRMS);
 	}
 	public double ValorFatorPot(double angulov, double anguloi) {
-		return Math.cos(angulov-anguloi);
+		return Math.cos(Math.toRadians(angulov)- Math.toRadians(anguloi));
 	}
 	
 	
