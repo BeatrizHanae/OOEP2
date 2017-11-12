e369a189a249263a26adb07fb56d395a1cee0e25
Beatriz Hanae <beatriz.hanae@gmail.com>
2017-11-12 10:03:08 -0200

    Classe para ativar o botão de Retornar ao Menu nas view das simulações

--- src/controller/ActionRetornaMenu.java
+++ src/controller/ActionRetornaMenu.java
@@ -1,40 +1,33 @@
-/*package controller;
+package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import view.Menu;
-import view.MenuInicial;
 
 public class ActionRetornaMenu implements ActionListener {
 
-	private JPanel painelFPF;
+	private JPanel painelVolta;
 	private JFrame telaInicial;
 	
-	public ActionRetornaMenu(JPanel painelFPF, JFrame telaInicial) {
-		this.painelFPF = painelFPF;
+	public ActionRetornaMenu(JPanel painelVolta, JFrame telaInicial) {
+		this.painelVolta = painelVolta;
 		this.telaInicial = telaInicial;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		painelFPF.setVisible(false);
+		painelVolta.setVisible(false);
 		try {
-			MenuInicial menu = new MenuInicial(janela);
-			menu.mostrarTela();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			new Menu(telaInicial);
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			System.out.println("Arquivo txt inexistente");
 		}
 	}
 
-}*/
+};
